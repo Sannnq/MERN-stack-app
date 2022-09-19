9e4e82703220eb17779c3a3503332337dbd6dfd2
Sannnq <founguesflorin@gmail.com>
2022-09-19 13:15:16 +0100

    display datas in front using axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function App() {
+  const [datas, setData] = useState({});
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/data").then((res) =>
+      setData({
+        ...datas,
+        ...res.data,
+      })
+
+    );
+  }, [])
+
+  let res = Object.values(datas)
+  console.log(typeof (datas))
   return (
     <div className="App">
-    <h1>MERN stack app</h1>
+      <h1>MERN stack app</h1>
+      <div>
+        {
+          res.map((e) => {
+            return <div>
+              <p>id: {e._id}</p>
+              <p>id: {e.name}</p>
+            </div>
+          })
+        }
+      </div>
     </div>
   );
 }
